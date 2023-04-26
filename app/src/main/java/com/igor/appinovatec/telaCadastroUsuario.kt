58c2619d5d44772f7397c5dc950573fb67ab9a2c
igor <igo12345marques@gmail.com>
2023-04-25 23:10:24 -0300

    Adicionado tela cadastro

---
+++ app/src/main/java/com/igor/appinovatec/telaCadastroUsuario.kt
@@ -0,0 +1,11 @@
+package com.igor.appinovatec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class telaCadastroUsuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_cadastro_usuario)
+    }
+}
